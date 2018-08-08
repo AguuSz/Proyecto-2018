07ecae30afd4c1c9a51b0a87dde3995a5505cf9a
AguuSz <agus.sepu2000@hotmail.com>
2018-08-08 18:46:53 -0300

    Agregado de test arduino

---
+++ Prueba.py
@@ -0,0 +1,40 @@
+import time
+import datetime
+
+class Reloj:
+
+    def pantalla (self):
+        """ Aca se define la pantalla, intentando simular la pantalla fisica. """
+
+        print("""
+        *************************************
+        *                                   *
+        *                                   *
+        *                                   *
+        *                                   *
+        *************************************
+            """)
+
+    def encendido (self):
+        """ Aca se define el encendido, intentando simular la pantalla fisica. """
+
+        print("""
+        *************************************
+        *          Sea bienvenido           *
+        *             Usuario               *
+        *                                   *
+        *             Cargando....          *
+        *************************************
+            """)
+
+    def timeDisplay(self):
+        """ Aca definimos lo que simularia la pantalla principal, con la fecha y hora """
+
+        print(datetime.datetime.now)
+
+
+
+
+smartwatch = Reloj()
+smartwatch.pantalla()
+smartwatch.timeDisplay()
